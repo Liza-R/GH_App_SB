d29808fbb65eaff7d8e842012ac773d7c10169b6
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-10-22 14:42:10 +0300

    Fix upload last or first avatar to table

--- GH_App/VC_AllUsers/Parsing/Loaders/SaveAvatarsTableLoader.swift
+++ GH_App/VC_AllUsers/Parsing/Loaders/SaveAvatarsTableLoader.swift
@@ -9,33 +9,6 @@ import Foundation
 import Alamofire
 import AlamofireImage
 
-/*
- let imageCache = AutoPurgingImageCache()
- let avatarImage = UIImage(data: data)!
-
- // Add
- imageCache.add(avatarImage, withIdentifier: "avatar")
-
- // Fetch
- let cachedAvatar = imageCache.image(withIdentifier: "avatar")
-
- // Remove
- imageCache.removeImage(withIdentifier: "avatar")
- */
-
-/*
- let imageView = UIImageView(frame: frame)
- let url = URL(string: "https://httpbin.org/image/png")!
-
- imageView.af.setImage(withURL: url)
- 
- let imageView = UIImageView(frame: frame)
- let url = URL(string: "https://httpbin.org/image/png")!
- let placeholderImage = UIImage(named: "placeholder")!
-
- imageView.af.setImage(withURL: url, placeholderImage: placeholderImage)
- */
-
 class LoadAvatarsAndSaveInfoForTable{
     func uploadAvatarsAndSaveInfo(ava_urls: [String], logins: [String]){
         var all_avatars: [NSData] = [],
@@ -46,11 +19,12 @@ class LoadAvatarsAndSaveInfoForTable{
             AF.request(j ,method: .get).response{ response in
                 switch response.result {
                     case .success(let responseData):
-                    
                     let ic = UIImage(data: responseData!, scale: 1) ?? .checkmark
                     imageCache.add(ic, withIdentifier: "\(j)")
                     c += 1
                     if c == ava_urls.count{
+                        let ics = imageCache.image(withIdentifier: "\(j)") ?? .checkmark
+                        all_avatars[i] = ics.pngData()! as NSData
                         SaveInfo().savingAllUsersInfo(logins: logins, avatars: all_avatars)
                     }else{
                         let ics = imageCache.image(withIdentifier: "\(j)") ?? .checkmark
