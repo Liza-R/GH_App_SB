ebb2ea06b813f144a50db14a6e6ef523c2add250
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-10-27 21:37:37 +0300

    Adding UI test for searchBar

--- GH_AppUITests/GH_AppUITests.swift
+++ GH_AppUITests/GH_AppUITests.swift
@@ -18,6 +18,23 @@ class GH_AppUITests: XCTestCase {
         app.launch()
     }
 
+    func testUserSearchBar(){
+        let inputUserNameSearchField = app.searchFields["Input user name"]
+        inputUserNameSearchField.tap()
+        let lKey = app/*@START_MENU_TOKEN@*/.keys["L"]/*[[".keyboards.keys[\"L\"]",".keys[\"L\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        lKey.tap()
+        let iKey = app/*@START_MENU_TOKEN@*/.keys["i"]/*[[".keyboards.keys[\"i\"]",".keys[\"i\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        iKey.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Done"]/*[[".keyboards",".buttons[\"done\"]",".buttons[\"Done\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        inputUserNameSearchField.tap()
+        let zKey = app.keys["z"]
+        zKey.tap()
+        let aKey = app.keys["a"]
+        aKey.tap()
+        let clearTextButton = inputUserNameSearchField.buttons["Clear text"]
+        clearTextButton.tap()
+    }
+    
     func testTableAllUsersSwipes(){
         let tablesQuery = app.tables,
             tableElement = tablesQuery.element
