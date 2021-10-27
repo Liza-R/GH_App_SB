ae8ca81032c49663785182c9fe3bf172e818aa97
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-10-27 14:46:08 +0300

    Speeding up tests execution for testing requests

--- GH_AppTests/GH_App_Request_Tests.swift
+++ GH_AppTests/GH_App_Request_Tests.swift
@@ -11,70 +11,66 @@ import XCTest
 
 class GH_App_Request_Tests: XCTestCase {
     
-    var sessionForTest: URLSession!
+    var createSessionForTest: URLSession!
+    let promiseStatus = "Completion handler invoked"
 
     override func setUp() {
         super.setUp()
-        sessionForTest = URLSession(configuration: URLSessionConfiguration.default)
+        createSessionForTest = URLSession(configuration: URLSessionConfiguration.default)
     }
 
     override func tearDown() {
-        sessionForTest = nil
+        createSessionForTest = nil
         super.tearDown()
     }
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    override func setUpWithError() throws {}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func tearDownWithError() throws {}
 
     func testAllUsersRequest(){
-        //given
+    //given
         let url = URL(string: URLs().allUsersURL)
-        let promise = expectation(description: "Status code: 200")
+        let promise = expectation(description: promiseStatus)
         validStatusCode200(url: url, promise: promise)
     }
     
     func testUserRequest(){
-        //given
+    //given
         let url = URL(string: "https://api.github.com/users/Liza-R")
-        let promise = expectation(description: "Status code: 200")
+        let promise = expectation(description: promiseStatus)
         validStatusCode200(url: url, promise: promise)
     }
     
     func testUserReposRequest(){
-        //given
+    //given
         let url = URL(string: "https://api.github.com/users/Liza-R/repos")
-        let promise = expectation(description: "Status code: 200")
+        let promise = expectation(description: promiseStatus)
         validStatusCode200(url: url, promise: promise)
     }
     
     func testSearchUserRequest(){
     //given
         let url = URL(string: "https://api.github.com/search/users?q=Liza-R")
-        let promise = expectation(description: "Status code: 200")
+        let promise = expectation(description: promiseStatus)
         validStatusCode200(url: url, promise: promise)
     }
     
     func validStatusCode200(url: URL?, promise: XCTestExpectation) {
+    //given
+        var statusCode: Int?,
+            responseError: Error?
     //when
-        let dataTask = sessionForTest.dataTask(with: url!){ data, response, error in
+        let dataTask = createSessionForTest.dataTask(with: url!){ data, response, error in
     //then
-            if let error = error{
-                XCTFail("Error: \(error.localizedDescription)")
-                return
-            }else if let statusCode = (response as? HTTPURLResponse)?.statusCode{
-                  if statusCode == 200 {
-                      promise.fulfill()
-                  }else{
-                      XCTFail("Status code: \(statusCode)")
-                  }
-            }
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            promise.fulfill()
         }
         dataTask.resume()
         waitForExpectations(timeout: 3.0, handler: nil)
+        
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
     }
 }
